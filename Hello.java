a1abff8288425360db6e16580700d3d9020f4298
Abu Sayeed <153213740+sayeedsoftd@users.noreply.github.com>
2024-01-23 06:45:52 -0800

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,27 @@
+package MyJavaProject.helloword;
+
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println(" _______         _________________");
+        System.out.println("|.    ()|       |_________________|");
+        System.out.println("|_____[]|      /|                 |");
+        System.out.println("|_____  |     / |     SAYEED      |");
+        System.out.println("|_____  |    /  |                 |");
+        System.out.println("|___(..)|   /   |_________________|");
+        System.out.println("|_______|  /    |_________________|");
+        System.out.println("|_______|  \'            ::");
+        System.out.println("|_______|   \'           ::");
+        System.out.println("|__[[]]:|    \'          ::");
+        System.out.println("              \'       ------");
+        System.out.println("               \'   _______________    ");
+        System.out.println("                \' /********  *****`      ");
+        System.out.println("                  /********    *****`      ");
+        System.out.println("                 /********      *****`      ");
+        System.out.println("                _______________________    ");
+
+
+
+    }
+}
