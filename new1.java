94cbf0cb973a3454a545f8cc68a712c266387d17
Abu Sayeed <153213740+sayeedsoftd@users.noreply.github.com>
2024-02-11 05:52:16 -0800

    Add files via upload

---
+++ new1.java
@@ -0,0 +1,24 @@
+package MyJavaProject.helloword;
+
+public class new1 {
+    public static void main(String[] args) {
+        Integer n1=100;
+        Integer n2=100;
+        Integer n3=500;
+        Integer n4=500;
+        if (n1==n2)
+        {
+            System.out.println("n1==n2");
+        }
+        else  {
+            System.out.println("n1!=n2");
+        }
+        if (n3==n4)
+        {
+            System.out.println("n3==n4");
+        }
+        else {
+            System.out.println("n3!=n4");
+        }
+    }
+}
